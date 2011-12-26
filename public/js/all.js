3bfbc73681b746f29167e82dafb8ce629b3e94c9
Zach Galant <zach.galant@gmail.com>
2011-12-25 16:43:07 -0800

    move jquery template markup into markup.js

--- public/js/all.js
+++ public/js/all.js
@@ -1,54 +1,18 @@
 D = console;
 cur_user = "";      // the logged in user, so we know who's viewing the page
 
-var group_listing_markup = "<a href='/group/${group._id}' class='group'><div>${group.name}</div></a>";
-var evnt_listing_markup = "<a href='/event/${event._id}' class='event'><div>${event.name}</div></a>";
-var ride_listing_markup = 
-                        "<div data-rid='${ride._id}' class='ride-listing'>\
-                            <div class='driver'>\
-                                <img class='user-image driver-image'\
-                                    src='http://graph.facebook.com/${driver.fbid}/picture?type=square' />\
-                                <div class='driver-info'>\
-                                    <div class='user-name driver-name'>${driver.first_name} ${driver.last_name}</div>\
-                                    <div class='clear'></div>\
-                                    <div class='ride-notes'>${ride.notes}</div>\
-                                </div>\
-                                <div class='join-ride-buttons'>\
-                                    <div class='hopin'>Hop in:</div>\
-                                    <button class='join-ride-there' type='submit' data-rid='${ride._id}'>Way There</button>\
-                                    <button class='join-ride-back' type='submit' data-rid='${ride._id}'>Way Back</button>\
-                                    <button class='join-ride-both' type='submit' data-rid='${ride._id}'>Both</button>\
-                                </div>\
-                            </div>\
-                            <div class='clear'></div>\
-                            <div class='riders-list way-there' data-rid='${ride._id}'>\
-                                <div class='rider'>Way There (${seats.there} spots) Leaving: ${leaving}</div>\
-                            </div>\
-                            <div class='riders-list way-back' data-rid='${ride._id}'>\
-                                <div class='rider'>Way Back (${seats.back} spots) Returning: ${returning}</div>\
-                            </div>\
-                        </div>";
-                        
-var rider_in_car_markup = "<div class='rider'>\
-                        <img class='user-image rider-image'\
-                            src='http://graph.facebook.com/${rider.fbid}/picture?type=square' />\
-                        <div class='user-name rider-name'>${rider.first_name} ${rider.last_name}</div>\
-                    </div><div class='clear'></div>";
-
-
-
 function msgReceived(msg){
     if (msg.path == window.location.pathname) {
         switch (msg.type) {
             case "join_group":
-                $.tmpl(group_listing_markup, msg).appendTo("#group-list");
+                $.tmpl(Markup.group_listing_markup, msg).appendTo("#group-list");
                 break;
             case "add_event":
-                $.tmpl(evnt_listing_markup, msg).appendTo("#event-list");
+                $.tmpl(Markup.evnt_listing_markup, msg).appendTo("#event-list");
                 break;
             case "add_car":
                 if (msg.success) {
-                    $.tmpl(ride_listing_markup, msg).appendTo("#ride-list");
+                    $.tmpl(Markup.ride_listing_markup, msg).appendTo("#ride-list");
                 } else {
                     Alerts.alert_user("You're already driving a car for this event.", msg.sender);
                 }
@@ -58,7 +22,7 @@ function msgReceived(msg){
                 var alert_text = "";
                 if (msg.way_there) {
                     if (msg.success.way_there) {
-                        $.tmpl(rider_in_car_markup, msg).appendTo(".way-there[data-rid='" + msg.ride._id + "']");
+                        $.tmpl(Markup.rider_in_car_markup, msg).appendTo(".way-there[data-rid='" + msg.ride._id + "']");
                     } else {
                         alert_text = "Sorry, there is no space on the way there.\n";
                     }
@@ -66,7 +30,7 @@ function msgReceived(msg){
                 }
                 if (msg.way_back) {
                     if (msg.success.way_back) {
-                        $.tmpl(rider_in_car_markup, msg).appendTo(".way-back[data-rid='" + msg.ride._id + "']");
+                        $.tmpl(Markup.rider_in_car_markup, msg).appendTo(".way-back[data-rid='" + msg.ride._id + "']");
                     } else {
                         alert_text = alert_text + "Sorry, there is no space on the way back.";
                     }
--- public/js/markup.js
+++ public/js/markup.js
@@ -1,3 +1,39 @@
 Markup = {};
 
 Markup.group_listing_markup = "<a href='/group/${group._id}' class='group'><div>${group.name}</div></a>";
+
+
+Markup.evnt_listing_markup = "<a href='/event/${event._id}' class='event'><div>${event.name}</div></a>";
+Markup.ride_listing_markup = 
+                        "<div data-rid='${ride._id}' class='ride-listing'>\
+                            <div class='driver'>\
+                                <img class='user-image driver-image'\
+                                    src='http://graph.facebook.com/${driver.fbid}/picture?type=square' />\
+                                <div class='driver-info'>\
+                                    <div class='user-name driver-name'>${driver.first_name} ${driver.last_name}</div>\
+                                    <div class='clear'></div>\
+                                    <div class='ride-notes'>${ride.notes}</div>\
+                                </div>\
+                                <div class='join-ride-buttons'>\
+                                    <div class='hopin'>Hop in:</div>\
+                                    <button class='join-ride-there' type='submit' data-rid='${ride._id}'>Way There</button>\
+                                    <button class='join-ride-back' type='submit' data-rid='${ride._id}'>Way Back</button>\
+                                    <button class='join-ride-both' type='submit' data-rid='${ride._id}'>Both</button>\
+                                </div>\
+                            </div>\
+                            <div class='clear'></div>\
+                            <div class='riders-list way-there' data-rid='${ride._id}'>\
+                                <div class='rider'>Way There (${seats.there} spots) Leaving: ${leaving}</div>\
+                            </div>\
+                            <div class='riders-list way-back' data-rid='${ride._id}'>\
+                                <div class='rider'>Way Back (${seats.back} spots) Returning: ${returning}</div>\
+                            </div>\
+                        </div>";
+                        
+Markup.rider_in_car_markup = "<div class='rider'>\
+                        <img class='user-image rider-image'\
+                            src='http://graph.facebook.com/${rider.fbid}/picture?type=square' />\
+                        <div class='user-name rider-name'>${rider.first_name} ${rider.last_name}</div>\
+                    </div><div class='clear'></div>";
+                    
+                    
